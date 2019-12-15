dac31a2c8c1436b460219caa0d9ee68e40930bc4
Ilia Belikov <ilia.belikov@samsung.com>
2019-12-15 14:02:27 +0300

    reverb frame

--- app/cpp/common/AudioEffects/ConvolutionReverb.h
+++ app/cpp/common/AudioEffects/ConvolutionReverb.h
@@ -8,14 +8,36 @@
 
 #include "AudioEffect.h"
 #include <GUI/Encoder.h>
+#include <common/Utils/AudioFile.h>
+#include <common/AudioEffects/FFTConvolver/Utilities.h>
+#include <common/AudioEffects/FFTConvolver/FFTConvolver.h>
 
 class ConvolutionReverb : public AudioEffect{
 
+    const size_t blockSize = 256;
+
     GUI::Encoder * drywet;
+
+    int sample_counter = 0;
+
+    AudioFile<float> ir;
+
+    std::vector<fftconvolver::Sample> inBuf;
+
+    fftconvolver::FFTConvolver convolver;
+
+    void processBuffer();
+
+    inline void pushBuffer(float sample) {
+
+    }
+
+    void swapBuffers();
+    void getResult();
 
 public:
 
-    ConvolutionReverb();
+    ConvolutionReverb(const char * ir);
 
     bool ARender(double beat, float * lsample, float * rsample) override;
 };
